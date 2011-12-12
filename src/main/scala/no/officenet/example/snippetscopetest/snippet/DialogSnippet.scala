15c3dcf254aa0351d998c22caa8d99d6845a4e50
Andreas Joseph Krogh <andreak@officenet.no>
2011-12-12 23:33:09 +0100

    Updated to work with new TransientSnippet, which solves all context-problems.

--- src/main/scala/no/officenet/example/snippetscopetest/snippet/DialogSnippet.scala
+++ src/main/scala/no/officenet/example/snippetscopetest/snippet/DialogSnippet.scala
@@ -2,18 +2,27 @@ package no.officenet.example.snippetscopetest.snippet
 
 import net.liftweb._
 import http._
+import js.JsCmds.SetHtml
 import util.Helpers._
+import java.util.Date
+import xml.Text
 
-class DialogSnippet {
+class DialogSnippet extends TransientSnippet {
 
 	val callbackFunc = callbackFuncVar.get
 	val listItemNum = listItemNumber.get
 
 	println("** Constructing new DialogSnippet for listItem: " + listItemNum + " " + this)
 
+	val timeId = nextFuncName
 	def render = {
 		println("* DialogSnippet.render for listItem: " + listItemNum + " " + this)
 		".itemNum *" #> listItemNum &
+		".time [id]" #> timeId &
+		".updateTime" #> SHtml.ajaxButton("Update time " + listItemNum, () => {
+			println("* DialogSnippet: Updating time for : " + listItemNum + " " + this)
+			SetHtml(timeId, Text(new Date().toString))
+		}) &
 		".updateList" #> SHtml.ajaxButton("Update listItem " + listItemNum, () => {
 			println("Updating background listItem: " + listItemNum + " by calling callbackFunc")
 			callbackFunc()
